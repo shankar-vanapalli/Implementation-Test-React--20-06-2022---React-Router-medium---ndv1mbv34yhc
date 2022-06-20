7d2de8a09865180a4544b326264e913e2980a9e5
Bhavani Shankar Vanapalli <40032387+shankar067@users.noreply.github.com>
2022-06-20 23:14:40 +0530

    Commited Check!

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
 
-
-export const BackgroundColorChanger = () =>{
-    return(
-       
-    )
-}
+export const BackgroundColorChanger = () => {
+  const params = useParams();
+  console.log(params.colorname);
+  return (
+    <main
+      style={{
+        height: "100vh",
+        backgroundColor: `${params.colorname}`,
+        fontSize: "30px"
+      }}
+    >
+      Background Color Changer
+    </main>
+  );
+};
--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
+export const Calculator = () => {
+  const [search, setSearch] = useSearchParams();
 
-export const Calculator = () =>{
+  //   console.log("Search params: " + search);
+  //   console.log(search.get("op"));
 
-    return(
-        
-    )
-}
+  // useEffect(() => {
+
+  // }, []);
+  useEffect(() => {
+    demo();
+  }, [search]);
+
+  function demo() {
+    const num1 = search.get("num1");
+    const num2 = search.get("num2");
+    const op = search.get("op");
+    var res;
+    if (op === "+") {
+      res = parseInt(num1) + parseInt(num2);
+    }
+    setSearch(() => res);
+  }
+
+  return (
+    <div>
+      Your calculation Result is <span id="calc-result">{search}</span>
+    </div>
+  );
+};
